fe65536f14e2f693d466a55e64fdff5d3074f21b
andresshare <andresshare@gmail.com>
2018-01-05 23:43:12 -0500

    add files

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,4 @@
-class Blog < ApplicationRecord
+class CreateFriendlyIdSlugs < ActiveRecord::Migration[5.1]
+  extend FriendlyId
+  friendly_id :title, use: :slugged
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+ #resources :portfolios,except[:show]
+  #get 'portfolio/:id', to: 'portfolios#show',as: 'portfolio_show'
+  #get 'about_me/pages#about'
+  #get 'contact/pages#contact'
+
   resources :portfolios
   get 'pages/home'
-
   get 'pages/about'
-
   get 'pages/contact'
 
   resources :blogs
